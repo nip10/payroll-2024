afccbf5ca61eb5b167e743a0b7fdba05a3ba3fd1
nip10 <diogocardoso92@outlook.com>
2024-09-26 01:15:29 +0100

    feat: add ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-V7GSQB25J0" />
     </html>
   );
 }
